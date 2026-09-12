cfc79a0bc79ee9f57776d9bc6d4ab5a7f9a1fa12
vbuarque <viniciusbuarque17@gmail.com>
2026-09-12 02:49:34 -0300

    chore: add TypeScript type-check script

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,10 +2,11 @@ import { useNavigate } from "react-router";
 
 export function ButtonPokemon() {
   const navigate = useNavigate();
+  const pokemonId: number = 25;
 
   function handleClick() {
     navigate("/Pokemon/:id");
   }
 
-  return <button onClick={handleClick}>PokeDetails</button>;
+  return <button onClick={handleClick}>PokeDetails{pokemonId}</button>;
 }
